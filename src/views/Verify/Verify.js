543fd986f244d3ee4d227200da800b78d5449dfb
Your Name <lex777fs@yandex.ru>
2018-09-24 15:07:26 +0300

    create Sign IN, Sign UP, Verify

---
+++ src/views/Verify/Verify.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+// import vsyakoy figni
+
+import withStyles from "@material-ui/core/styles/withStyles";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import CardFooter from "components/Card/CardFooter.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+
+const styles = {
+  cardCategoryWhite: {
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0"
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none"
+  }
+};
+
+
+function Verify (props) {
+  const {classes} = props;
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={8}>
+          <Card>
+            <CardHeader color="primary">
+              <h4 className={classes.cardTitleWhite}>
+                Register with another APP</h4>
+              <p className={classes.cardCategoryWhite}>
+                Please, enter your email and password</p>
+            </CardHeader>
+            <CardBody>
+              <GridContainer>
+                <GridItem xs={10} sm={5} md={8}>
+                  <CustomInput
+                    labelText = "email adress"
+                    id = "email"
+                    formControlProps={{
+                      fullWidth: true
+                    }}
+                  />
+                </GridItem>
+                <GridItem xs={10} sm={5} md={8}>
+                  <CustomInput
+                    labelText = "Verify code"
+                    formControlProps={{
+                      fullWidth: true
+                    }}
+                  />
+                </GridItem>
+              </GridContainer>
+            </CardBody>
+            <CardFooter>
+              <GridContainer>
+                <GridItem xs={10} sm={5} md={8}>
+                  <Button color="primary" >Verify Email</Button>
+                </GridItem>
+                <GridItem xs={10} sm={5} md={8}>
+                  <a
+                    href="#"
+                    className={classes.cardLink}
+                    onClick={(e)=>e.preventDefault()}
+                  >
+                    first time user? Sign Up
+                  </a>
+                </GridItem>
+              </GridContainer>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
+  )
+}
+export default withStyles(styles)(Verify);
