8ad1fc8a85291c81f9190e9585a2687ce41d9b01
Tony <sarana.av93@gmail.com>
2018-10-07 12:03:23 +0300

    some fix SignUp

--- server/config/DB.js
+++ server/config/DB.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DB: 'mongodb://localhost:27017/voterDB'
-  //DB: 'mongodb://simpleUser:112233q@ds219983.mlab.com:19983/voters'
+  //DB: 'mongodb://localhost:27017/voterDB'
+  DB: 'mongodb://simpleUser:112233q@ds219983.mlab.com:19983/voters'
 };
